998298d635802b648d1c9443be41a85cfe2efafa
Luke Brandon <brandonlukeb@gmail.com>
2022-08-17 16:11:49 +0800

    Got some UI stuff working

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button ui1Yes;
+    Button ui1No;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.firstui);
+
+        ui1Yes = findViewById(R.id.ui1Yes);
+
+        ui1Yes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                setContentView(R.layout.secondui);
+            }
+        });
     }
+
+
 }
