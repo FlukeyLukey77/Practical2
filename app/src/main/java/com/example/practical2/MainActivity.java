46e8e7f2cd5bc70b1a766fe9a6aacf70084cf60f
Luke Brandon <brandonlukeb@gmail.com>
2022-08-22 18:23:42 +0800

    Got the app working up to the tablet part

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.practical2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity
 {
     Button ui1Yes;
     Button ui1No;
+    TextView ageQuestion;
+    ConstraintLayout mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +27,11 @@ public class MainActivity extends AppCompatActivity
 
         ui1Yes = findViewById(R.id.ui1Yes);
         ui1No = findViewById(R.id.ui1No);
+        ageQuestion = findViewById(R.id.ageQuestion);
+        mainView = findViewById(R.id.Main);
+
+
+        Snackbar ageMessage = Snackbar.make(mainView, "Age too low! Results will not be valid.", BaseTransientBottomBar.LENGTH_SHORT);
 
         ui1Yes.setOnClickListener(new View.OnClickListener()
         {
@@ -36,7 +48,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                setContentView(R.layout.thirdui);
+                ageMessage.show();
             }
         });
 
